2b73b794f3f8e6ac768566d744790a1c6cbf455e
khaidprogramming <khalidmiya399@gmail.com>
2024-06-24 08:24:01 +0545

    added airport seeders and associated city and airport

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const CityRepository = require('./repository/city-repository')
 const ApiRoutes = require('./routes/index');
+const db = require("./models");
 
 const setupAndStartServer = async () => {
   //create the express object
@@ -16,6 +17,9 @@ const setupAndStartServer = async () => {
 
   app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
+    if(process.env.SYNC_DB){
+      db.sequelize.sync({alter:true});
+    }
 
   });
 };
---
+++ src/seeders/20240624003255-add-aiports.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Airports', [
+    {
+      name: 'Chitwan airport',
+      cityId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'A airport',
+      cityId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'B airport',
+      cityId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Pokhara airport',
+      cityId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
